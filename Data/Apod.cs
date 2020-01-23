26e14015f602410344947ee192fe22f3a1d76339
MrFrawsty <kennyjoec@gmail.com>
2020-01-23 17:50:02 -0600

    Add project files.

---
+++ Data/Apod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NASA_App.Data
+{
+    public class Apod
+    {
+        public string CopyRight { get; set; }
+
+        public string Date { get; set; }
+
+        public string Explanation { get; set; }
+
+        public Uri HDUri { get; set; }
+
+        public Uri Url { get; set; }
+
+        public string Media_Type { get; set; }
+
+
+    }
+}
---
+++ Data/ApodService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NASA_App.Data
+{
+    public class ApodService
+    {
+        
+        static readonly HttpClient httpClient = new HttpClient();
+
+        public async Task<Apod> GetApodAsync(string path)
+        {
+            Apod apod = null;
+
+            HttpResponseMessage response = await httpClient.GetAsync(path);
+            if(response.IsSuccessStatusCode)
+            {
+                apod = await response.Content.ReadAsAsync<Apod>();
+            }
+
+                return apod;
+            
+        }
+
+        public async Task<Apod> GetNextApodAsync(string path, string date)
+        {
+            Apod nextApod = null;
+
+            HttpResponseMessage response = await httpClient.GetAsync(path + "&date=" + date);
+            if (response.IsSuccessStatusCode)
+            {
+                nextApod = await response.Content.ReadAsAsync<Apod>();
+            }
+
+            return nextApod;
+        }
+
+
+       public DateTime NextDay(DateTime selectedDate)
+        {
+            if(selectedDate < DateTime.Now)
+            selectedDate = selectedDate.AddDays(01);
+
+            return selectedDate;
+        }
+
+        public DateTime PreviousDay(DateTime selectedDate)
+        {
+            selectedDate = selectedDate.AddDays(-1);
+
+            return selectedDate;
+        }
+
+    }
+}
