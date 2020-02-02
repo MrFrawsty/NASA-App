88a3281a98d2bee2c942dec0678530d488ad83a7
MrFrawsty <kennyjoec@gmail.com>
2020-02-01 20:50:32 -0600

    Created initial rover model class and class for connecting to API

---
+++ Data/Rover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NASA_App.Data
+{
+    public class Rover
+    {
+        public string Full_Name { get; set; }
+
+        public Uri Img_Src { get; set; }
+
+    }
+}
---
+++ Data/RoverService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NASA_App.Data
+{
+    public class RoverService
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+
+        public async Task<Rover> GetRoverDataAsync(string path)
+        {
+            Rover rover = null;
+
+            HttpResponseMessage response = await httpClient.GetAsync(path);
+            if(response.IsSuccessStatusCode)
+            {
+                rover = await response.Content.ReadAsAsync<Rover>();
+            }
+
+            return rover;
+        }
+
+    }
+}
