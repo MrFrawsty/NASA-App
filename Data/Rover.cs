7acbf618fae62fcae0a40025055bbdd86849bc47
MrFrawsty <kennyjoec@gmail.com>
2020-02-02 15:00:29 -0600

    Began working on parsing data returned from rover api

--- Data/Rover.cs
+++ Data/Rover.cs
@@ -7,9 +7,12 @@ namespace NASA_App.Data
 {
     public class Rover
     {
+        public int rover_id { get; set; }
+        
         public string Full_Name { get; set; }
 
         public Uri Img_Src { get; set; }
 
+       
     }
 }
