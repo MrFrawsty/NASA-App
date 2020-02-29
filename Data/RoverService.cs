85e0e0069533ca46244945f88d25cd58f970cd0a
MrFrawsty <kennyjoec@gmail.com>
2020-02-28 19:59:08 -0600

    Renamed some properties to avoid confusion. Temporarily fixed bug causing index out of range error, will add fix that gives user feedback next commit.

--- Data/RoverService.cs
+++ Data/RoverService.cs
@@ -9,26 +9,31 @@ namespace NASA_App.Data
     public class RoverService
     {
         static readonly HttpClient httpClient = new HttpClient();
-        public List<Photo> returnedRovers = new List<Photo>();
-        static int roverPosition;
+        public List<Photo> returnedPhotos { get; set; }
+        public int RoverPosition { get; set; }
         public Photo CurrentPhoto { get; set; }
         public Rover Rover { get; set; }
+        
     
 
         public async Task<Rover> GetRoverDataAsync(string path)
         {
             Camera camera; 
              Rover = null;
+            RoverPosition = 0;
 
             HttpResponseMessage response = await httpClient.GetAsync(path);
             if(response.IsSuccessStatusCode)
             {
+                
                 Rover = await response.Content.ReadAsAsync<Rover>();
                 camera = await response.Content.ReadAsAsync<Camera>();
 
-                returnedRovers = Rover.Photos;
-                CurrentPhoto = returnedRovers[roverPosition];
-
+                returnedPhotos = Rover.Photos;
+                if (returnedPhotos.Count > 0)
+                {
+                    CurrentPhoto = returnedPhotos[RoverPosition];
+                }
             }
 
             return Rover;
@@ -36,34 +41,45 @@ namespace NASA_App.Data
 
         public void NextPicture()
         {
-            if (roverPosition < returnedRovers.Count -1)
+            if (RoverPosition < returnedPhotos.Count -1)
             {
-                roverPosition++;
-                CurrentPhoto = returnedRovers[roverPosition];
+                RoverPosition++;
+                CurrentPhoto = returnedPhotos[RoverPosition];
             }
 
             else
             {
                
-                CurrentPhoto = returnedRovers[roverPosition];
+                CurrentPhoto = returnedPhotos[RoverPosition];
             }
         }
 
         public void PreviousPicture()
         {
-            if (roverPosition > 0)
+            if (RoverPosition > 0)
             {
-                roverPosition--;
-                CurrentPhoto = returnedRovers[roverPosition];
+                RoverPosition--;
+                CurrentPhoto = returnedPhotos[RoverPosition];
             }
 
             else
             {
             
-                CurrentPhoto = returnedRovers[roverPosition];
+                CurrentPhoto = returnedPhotos[RoverPosition];
             }
 
         }
+
+        public DateTime GetRandomDate()
+        {
+            Random random = new Random();
+            DateTime start = new DateTime(2013, 1, 1);
+            DateTime end = new DateTime(2015, 12, 31);
+            int range = (end - start).Days;
+            return start.AddDays(random.Next(range));
+
+        }
+
 
     }
 
